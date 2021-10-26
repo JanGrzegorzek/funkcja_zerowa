aecfb6fbb68836c5b4f52f44491d1d9f7af46efb
Jan Grzegorzek <janekgrzegorzek@gmail.com>
2021-10-26 15:16:11 +0200

    ver1.0

--- zerowe.cpp
+++ zerowe.cpp
@@ -2,7 +2,49 @@
 
 using namespace std;
 
+void zerowa(float a, float b)
+{
+	float wartosc;
+
+	cout << "\n\nTwoja funkcja ma postac "<< a <<"x + (" << b << ") = 0"<< endl;
+	
+	/*if(a==0 && b!=0)
+	{
+		cout<<"\n\nNie mozna utworzyc funkji liniowej z podanymi parametrami!"<< endl;
+	}
+	
+	if(a==0 && b==0)
+	{
+		cout << "\n\nFunkcja ma nieskonczenie wiele miejsc zerowych"<<endl;
+	}
+	*/
+	if(a=!0)
+	{
+		if(b==0)
+		{
+			wartosc = 0;
+		}
+		
+		else
+		{
+			wartosc = -b/a;	
+		}
+		 
+		cout << "\n\nMiejsce zerowe twojej funkcji to: " << wartosc << endl;
+	}		
+} 
+
 int main()
 {
+	float a, b;
+	cout << "!Program do obliczania mejsc zerowych funkcji liniowej!\n\n";
+	cout << "Podaj a=";
+	cin >> a;
+	cout << "Podaj b=";
+	cin >> b;
+	
+	zerowa(a,b);
+	
+	
 	return 0;
 }
